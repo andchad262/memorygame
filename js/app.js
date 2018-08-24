a484db965e8f0114f31831ad52346c3342e58917
Chad <andchadtx@gmail.com>
2018-08-23 22:41:56 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,329 +1,196 @@
-let deck = document.querySelector('.deck');
-let allCards;
-let openCards = [];
-let matches = 0;
-
-let clicks = 0;
-const moveCounter = document.querySelector('.moves-num');
-let moves = 0;
-let noMatchMoves = 0;
-let stars = 3;
-const restartBtn = document.querySelector('.restart');
-
-const winModal = document.querySelector('.win-modal');
-const playAgainBtn = document.querySelector('.play-again');
-
-let timerText = document.querySelector('.timer');
-let seconds = 0;
-let minutes = 0;
-let hours = 0;
-let t;
-let formattedTime = '0';
-let totalGameTime = '0';
-
-
-/*
-*
-* GAMEPLAY
-*
-*/
-
-newGame();
-
-
-/*
-*
-* EVENT LISTENERS
-*
-*/
-
-// Determine the first click on the deck to start the timer
-deck.addEventListener('click', function () {
-  clicks++;
-  if (clicks == 1){
-    timer();
+var deck = [
+  "fa-diamond",
+  "fa-diamond",
+  "fa-paper-plane-o",
+  "fa-paper-plane-o",
+  "fa-anchor",
+  "fa-anchor",
+  "fa-bolt",
+  "fa-bolt",
+  "fa-cube",
+  "fa-cube",
+  "fa-leaf",
+  "fa-leaf",
+  "fa-bicycle",
+  "fa-bicycle",
+  "fa-bomb",
+  "fa-bomb"
+];
+ var open = [];
+var matched = 0;
+var moveCounter = 0;
+var numStars = 3;
+var timer = {
+  seconds: 0,
+  minutes: 0,
+  clearTime: -1
+};
+ // Difficulty settings (max number of moves for each star)
+var hard = 15;
+var medium = 20;
+ var modal = $("#win-modal");
+ //Start time first card is clicked
+ //Start timer
+var startTimer = function() {
+  if (timer.seconds === 59) {
+    timer.minutes++;
+    timer.seconds = 0;
+  } else {
+    timer.seconds++;
   }
-});
-
-// Restart the game when the win modal button is clicked
-playAgainBtn.addEventListener('click', function () {
-  newGame();
-  winModal.style.display = "none";
-});
-
-// Restart the game when the restart button is clicked
-restartBtn.addEventListener('click', function () {
-  stopTimer();
-  newGame();
-});
-
-
-/*
-*
-* FUNCTIONS
-*
-*/
-
-
-/* ------------------------ Gameplay ------------------------*/
-
-
-// Initalize the board and handle gameplay
-function newGame(){
-
-  newGameboard();
-
-  // Handle game play
-  allCards.forEach(function(card) {
-    card.addEventListener('click', function () {
-      if(isFippable(card)){
-        openCard(card);
-        addToOpenCardList(card);
-        if (openCards.length == 2){
-          increaseMoveCounter();
-          if (isMatch(openCards[0], openCards[1])){
-            createMatch(openCards[0], openCards[1]);
-            openCards = [];
-            matches++;
-            if(isWinner()){
-              createWinModal();
-            }
-          }else{
-            closeCards(openCards[0], openCards[1]);
-            openCards = [];
-            noMatchMoves++;
-            updateStarStatus();
-          }
-        }
-      }
-    });
+   // Ensure that single digit seconds are preceded with a 0
+  var formattedSec = "0";
+  if (timer.seconds < 10) {
+    formattedSec += timer.seconds;
+  } else {
+    formattedSec = String(timer.seconds);
+  }
+   var time = String(timer.minutes) + ":" + formattedSec;
+  $(".timer").text(time);
+};
+// Resets timer state and restarts timer
+function resetTimer() {
+  clearInterval(timer.clearTime);
+  timer.seconds = 0;
+  timer.minutes = 0;
+  $(".timer").text("0:00");
+   timer.clearTime = setInterval(startTimer, 1000);
+}
+ // Randomizes cards on board and updates card HTML
+function updateCards() {
+  deck = shuffle(deck);
+  var index = 0;
+  $.each($(".card i"), function() {
+    $(this).attr("class", "fa " + deck[index]);
+    index++;
   });
 }
-
-// Reset all values, create cards, and start the timer, move counter, & star rating
-function newGameboard(){
-  clicks = 0;
-  resetTimer();
-  noMatchMoves = 0;
-  resetStars();
-  matches = 0;
-  openCards = [];
-  deck.innerHTML = ' ';
-  createCardsHTML();
-  allCards = document.querySelectorAll('.card');
-  inializeMoveCounter();
+ // Toggles win modal on
+function showModal() {
+  modal.css("display", "block");
 }
-
-// Dertermine if the user has won the game
-function isWinner(){
-  if (matches == 8){
-    stopTimer();
+ // Removes last start from remaining stars, updates modal HTML
+function removeStar() {
+  $(".fa-star")
+    .last()
+    .attr("class", "fa fa-star-o");
+  numStars--;
+  $(".num-stars").text(String(numStars));
+}
+ // Restores star icons to 3 stars, updates modal HTML
+function resetStars() {
+  $(".fa-star-o").attr("class", "fa fa-star");
+  numStars = 3;
+  $(".num-stars").text(String(numStars));
+}
+ // Updates number of moves in the HTML, removes star if necessary based on difficulty variables
+function updateMoveCounter() {
+  $(".moves").text(moveCounter);
+   if (moveCounter === hard || moveCounter === medium) {
+    removeStar();
+  }
+}
+ // Checks if card is a valid move (if it not currently matched or open)
+function isValid(card) {
+  return !(card.hasClass("open") || card.hasClass("match"));
+}
+ // Returns whether or not currently open cards match
+function checkMatch() {
+  if (open[0].children().attr("class") === open[1].children().attr("class")) {
     return true;
-  }else{
+  } else {
     return false;
   }
 }
-
-
-/* ------------------------ Cards ------------------------*/
-
-
-// Creates randomly shuffled cards on the gameboard
-function createCardsHTML(){
-  const cards = ['fa-anchor', 'fa-anchor',
-  'fa-bicycle', 'fa-bicycle',
-  'fa-bolt', 'fa-bolt',
-  'fa-bomb', 'fa-bomb',
-  'fa-cube', 'fa-cube',
-  'fa-diamond', 'fa-diamond',
-  'fa-leaf', 'fa-leaf',
-  'fa-paper-plane-o', 'fa-paper-plane-o'];
-  const cardsHTML = [];
-  deck = document.querySelector('.deck');
-
-  cards.forEach(function(card){
-    cardsHTML.push('<li class="card"><i class="fa ' + card + '"></i></li>');
-  });
-  shuffle(cardsHTML);
-  deck.innerHTML = cardsHTML.join('');
+ // Returns win condition
+function hasWon() {
+  if (matched === 16) {
+    return true;
+  } else {
+    return false;
+  }
 }
-
-// Shuffle function from http://stackoverflow.com/a/2450976
+ // Sets currently open cards to the match state, checks win condition
+var setMatch = function() {
+  open.forEach(function(card) {
+    card.addClass("match");
+  });
+  open = [];
+  matched += 2;
+   if (hasWon()) {
+    clearInterval(timer.clearTime);
+    showModal();
+  }
+};
+ // Sets currently open cards back to default state
+var resetOpen = function() {
+  open.forEach(function(card) {
+    card.toggleClass("open");
+    card.toggleClass("show");
+  });
+  open = [];
+};
+ // Sets selected card to the open and shown state
+function openCard(card) {
+  if (!card.hasClass("open")) {
+    card.addClass("open");
+    card.addClass("show");
+    open.push(card);
+  }
+}
+ // Resets all game state variables and resets all required HTML to default state
+var resetGame = function() {
+  open = [];
+  matched = 0;
+  moveCounter = 0;
+  resetTimer();
+  updateMoveCounter();
+  $(".card").attr("class", "card");
+  updateCards();
+  resetStars();
+};
+ // Handles primary game logic of game
+var onClick = function() {
+  if(timer.seconds == 0 && timer.minutes == 0){
+    resetTimer();
+  }
+  if (isValid($(this))) {
+    if (open.length === 0) {
+      openCard($(this));
+    } else if (open.length === 1) {
+      openCard($(this));
+      moveCounter++;
+      updateMoveCounter();
+       if (checkMatch()) {
+        setTimeout(setMatch, 300);
+      } else {
+        setTimeout(resetOpen, 700);
+      }
+    }
+  }
+};
+ // Resets game state and toggles win modal display off
+var playAgain = function() {
+  resetGame();
+  modal.css("display", "none");
+};
+ // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
-
-  while (currentIndex !== 0) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+   while (currentIndex !== 0) {
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
     temporaryValue = array[currentIndex];
     array[currentIndex] = array[randomIndex];
     array[randomIndex] = temporaryValue;
   }
-  return array;
+   return array;
 }
-
-// Visually "open" (flip over) a card
-function openCard(card){
-  card.classList.add('open', 'show');
-}
-
-// Keep track of which cards are "open" (flipped)
-function addToOpenCardList(card){
-  openCards.push(card);
-}
-
-// Determine if a card can be "opened" (flipped)
-function isFippable(card){
-  if(!card.classList.contains('open') && !card.classList.contains('show') &&
-  !card.classList.contains('match') && openCards.length < 2){
-    return true;
-  }else{
-    return false;
-  }
-}
-
-// Determine if a cards are a match
-function isMatch(cardA, cardB){
-  if (cardA.innerHTML == cardB.innerHTML){
-    return true;
-  }else{
-    return false;
-  }
-}
-
-// Indicate a successful match & create a match
-function createMatch(cardA, cardB){
-  cardA.classList.add('match');
-  cardB.classList.add('match');
-}
-
-// Indicate an unsuccessful match & "close" card (flip it back over)
-function closeCards(cardA, cardB){
-  setTimeout(function(){
-    cardA.classList.add('no-match');
-    cardB.classList.add('no-match');
-  }, 300);
-
-  setTimeout(function(){
-    cardA.classList.remove('open', 'show');
-    cardB.classList.remove('open', 'show');
-    cardA.classList.remove('no-match');
-    cardB.classList.remove('no-match');
-  }, 1200);
-}
-
-
-/* ------------------------ Score Panel ------------------------*/
-
-
-// Determine the star rating based on the number of unsuccessful matches
-function updateStarStatus(){
-  if (noMatchMoves == 5){
-    removeStar();
-  }else if (noMatchMoves == 10){
-    removeStar();
-  }
-}
-
-function removeStar(){
-  const star = document.querySelector('.fa-star');
-  star.classList.remove('fa-star');
-  star.classList.add('fa-star-o');
-  stars--;
-}
-
-function addStar(){
-  const star = document.querySelector('.fa-star-o');
-  star.classList.remove('fa-star-o');
-  star.classList.add('fa-star');
-  stars++;
-}
-
-function resetStars(){
-  const maxStars = 3;
-  const starsNeeded = (maxStars - stars);
-  for (let i = 0; i < starsNeeded; i++){
-    addStar();
-  }
-}
-
-function inializeMoveCounter(){
-  moves = 0;
-  moveCounter.innerHTML = moves;
-}
-
-function increaseMoveCounter(){
-  moves++;
-  moveCounter.innerHTML = moves;
-}
-
-//Timer functions from https://jsfiddle.net/Daniel_Hug/pvk6p/
-function timer() {
-  t = setTimeout(addOneSec, 1000);
-}
-
-function addOneSec(){
-  seconds++;
-  if (seconds >= 60) {
-    seconds = 0;
-    minutes++;
-    if (minutes >= 60) {
-      minutes = 0;
-      hours++;
-    }
-  }
-
-  formattedTime = (hours ? (hours > 9 ? hours : '0' + hours) : '00') + ':'
-  + (minutes ? (minutes > 9 ? minutes : '0' + minutes) : '00') + ':'
-  + (seconds > 9 ? seconds : '0' + seconds);
-
-  timerText.textContent = formattedTime
-
-  timer();
-}
-
-function stopTimer(){
-  clearTimeout(t);
-  totalGameTime = timerText.textContent;
-}
-
-function resetTimer(){
-  timerText.textContent = '00:00:00';
-  seconds = 0;
-  minutes = 0;
-  hours = 0;
-  formattedTime = '0';
-  totalGameTime = '0';
-}
-
-
-/* ------------------------ Win Modal ------------------------*/
-
-
-function createWinModal(){
-  getResultTime();
-  getResultMoves();
-  getResultStars();
-  winModal.style.display = 'block';
-}
-
-// Get the total time it took the user to play the game
-function getResultTime(){
-  let resultTime = document.querySelector('.timer-result');
-  resultTime.textContent = timerText.textContent;
-}
-
-// Get the total number of moves it took the user to play the game
-function getResultMoves(){
-  let resultMoves = document.querySelector('.moves-result-num');
-  resultMoves.textContent  = moveCounter.textContent;
-}
-
-// Get the star rating based on the user's gameplay
-function getResultStars(){
-  let resultStars = document.querySelector('.stars-result');
-  let stars = document.querySelector('.stars');
-  console.log(stars.innerHTML);
-  console.log(resultStars.innerHTML);
-  resultStars.innerHTML  = stars.innerHTML;
-}
+ /* Initalize event listeners */
+ $(".card").click(onClick);
+$(".restart").click(resetGame);
+$(".play-again").click(playAgain);
+ // Provides a randomized game board on page load
+$(updateCards);
